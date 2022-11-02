00eeb8ffd6acebcb7856bc9e0538aaaf3ee8cfaf
Pedro <pdiazstam@gmail.com>
2022-11-02 18:12:54 -0500

    Cambios según lo visto en clase con Abilio

--- src/main/java/com/nttdata/bootcamp/QueryBalanceService/application/QueryBalanceController.java
+++ src/main/java/com/nttdata/bootcamp/QueryBalanceService/application/QueryBalanceController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -21,24 +22,28 @@ public class QueryBalanceController {
     private final IQueryBalanceService service;
 
     // Consulta de movimientos
+    @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(path = "/passive/movement/{id}")
+    @GetMapping(path = "/passive/movement/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<OperationResponse> getMovementPassive(@PathVariable String id){
         return service.getMovementPassive(id);
     }
+    @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(path = "/active/movement/{id}")
+    @GetMapping(path = "/active/movement/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<MovementResponse> getMovementActive(@PathVariable String id){
         return service.getMovementActive(id);
     }
     // Consultas de saldo
+    @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(path = "/passive/{id}")
+    @GetMapping(path = "/passive/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Mono<CustomerPassiveProductResponse> getBalancePassive(@PathVariable String id){
         return service.getBalancePassive(id);
     }
+    @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(path = "/active/{id}")
+    @GetMapping(path = "/active/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Mono<CustomerActiveProductResponse> getBalanceActive(@PathVariable String id){
         return service.getBalanceActive(id);
     }
