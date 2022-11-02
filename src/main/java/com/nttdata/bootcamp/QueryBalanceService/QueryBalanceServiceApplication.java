2e78e288dd01eaff041216747d1380d366f9cd96
Pedro <pdiazstam@gmail.com>
2022-11-02 00:02:51 -0500

    Configuración para el discovery service

--- src/main/java/com/nttdata/bootcamp/QueryBalanceService/QueryBalanceServiceApplication.java
+++ src/main/java/com/nttdata/bootcamp/QueryBalanceService/QueryBalanceServiceApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.bootcamp.QueryBalanceService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class QueryBalanceServiceApplication {
 
 	public static void main(String[] args) {
